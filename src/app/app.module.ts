f4c739af6fa171ec9fb7c67d7432c71f62360307
Raphael M Arantes <raphael.arantes@gft.com>
2023-07-26 15:30:41 -0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DocumentComponent } from './components/document/document.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DocumentComponent
   ],
   imports: [
     BrowserModule,
